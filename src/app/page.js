9315c7a83e446ed61d091b6899ab771d69d57c41
souravmsi <hudasourav@gmail.com>
2023-10-05 22:48:11 +0530

    api fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import PopularCarousel from "@/components/molecules/PopularCarousel";
 import { getData } from "@/utils/api";
 
 export default async function Home() {
-  const menData = await getData("http://localhost:8000/api/men-products");
-  const womenData = await getData("http://localhost:8000/api/women-products");
-  const heroData = await getData("http://localhost:8000/api/hero-components");
+  const menData = await getData("https://koovs-backend.onrender.com/api/men-products");
+  const womenData = await getData("https://koovs-backend.onrender.com/api/women-products");
+  const heroData = await getData("https://koovs-backend.onrender.com/api/hero-components");
   const newArrivalData = await getData(
-    "http://localhost:8000/api/new-arrivals"
+    "https://koovs-backend.onrender.com/api/new-arrivals"
   );
 
   return (
